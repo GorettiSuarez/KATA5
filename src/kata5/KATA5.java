7e7c385de3df33cc89877dfa2d0a0589f09623e0
Goretti <Goretti@Goretti>
2015-11-12 09:35:03 +0000

    KATA5: Lee de una base de datos de Oracle (Esta es de Enrique)

--- src/kata5/KATA5.java
+++ src/kata5/KATA5.java
@@ -10,15 +10,19 @@ import java.sql.*;
 public class KATA5 {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Class.forName("org.sqlite.JDBC");
-        Connection c = DriverManager.getConnection("jdbc:sqlite:KATADB");
-
+        //Class.forName("org.sqlite.JDBC");
+        //Connection c = DriverManager.getConnection("jdbc:sqlite:KATADB");
+        
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection c = DriverManager.getConnection("jdbc:oracle:thin:@10.22.143.90:1521:orcl","system","orcl");
+        
         Statement stmt = c.createStatement();
         ResultSet rs = stmt.executeQuery("SELECT * FROM PEOPLE");
         
         while (rs.next()){
-            System.out.println("ID = " +rs.getInt("ID"));
-            System.out.println("NAME = " +rs.getString("NAME"));
+            System.out.print("ID = " +rs.getInt("ID"));
+            System.out.print("-->");
+            System.out.println(" NAME = " +rs.getString("NAME"));
         }
         
         rs.close();
